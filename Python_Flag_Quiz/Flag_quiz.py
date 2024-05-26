9d20a60f8d037f6bfa2494d1e2ccd46ebae0bf5c
Vibert100 <163373003+Vibert100@users.noreply.github.com>
2024-05-27 01:30:34 +1000

    Added buttons, added question generator function and added basic check answer function.
    Modified the dictionary code to have the answers on the 4th index this is so that the answer can be checked easily. Added buttons, added question generator function and added basic check answer function.

--- Python_Flag_Quiz/Flag_quiz.py
+++ Python_Flag_Quiz/Flag_quiz.py
@@ -7,17 +7,33 @@ main = Tk()
 Score = 0
 
 #Dictionary that makes it easy to add options and images associated to a specific question
-Flag_Quiz_Options = {
-    "Question 1": ["Australian_Flag.jpeg","Australia","New Zealand","Greece","Sweden"],
-    "Question 2": ["Spanish_Flag.jpeg","Italy","China","Spain","Egypt"]
+Flag_Quiz = {
+    "Question 1": ["Australia","New Zealand","Greece","Sweden",0,"Australian_Flag.jpeg"],
+    "Question 2": ["Italy","China","Spain","Egypt",2,"Spanish_Flag.jpeg"]
     #More questions will be added later, there are only 2 right now so that I can test the next question function later
 }
 
 
+
+#Called when the start button pressed, starts the quiz
 def Start_Quiz():
     for i in main.winfo_children():
         i.pack_forget()
+    
+    Question_Generator()
 
+#Checks if the button clicked is the correct answer
+def Check_Answer(i):
+    #
+    if i == list(Flag_Quiz.items())[0][1][4]:
+        print("Correct answer")
+
+#Generates questions
+def Question_Generator():
+    #Creates 4 buttons
+    for i in range(4):
+        Button_Options = Button(main, text="test", command=lambda i=i:Check_Answer(i))
+        Button_Options.pack(anchor=W)
 
 Quiz_title = Label(main, text="Flag Quiz")
 Start_Button = Button(main, text="Start Button", command=Start_Quiz)
